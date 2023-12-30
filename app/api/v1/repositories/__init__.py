d9a99c65d7057ad7665af6fad706459dc68d73b7
Valentyn Malenchak <32437654+valentyn-malenchak@users.noreply.github.com>
2023-12-30 16:27:53 +0200

    [#0007]: Implement MongoDB transactions (#6)

--- app/api/v1/repositories/__init__.py
+++ app/api/v1/repositories/__init__.py
@@ -5,12 +5,15 @@ from typing import Any, Dict, List
 
 from bson import ObjectId
 from fastapi import Depends
+from motor.motor_asyncio import AsyncIOMotorClientSession
 
 from app.services.mongo.service import MongoDBService
 
 
 class BaseRepository(abc.ABC):
     """Base repository for handling data access operations."""
+
+    _collection_name: str | None = None
 
     def __init__(self, mongo: MongoDBService = Depends()) -> None:
         """Initializes the BaseRepository.
@@ -24,11 +27,15 @@ class BaseRepository(abc.ABC):
         self._mongo = mongo
 
     @abc.abstractmethod
-    async def get_item_by_id(self, id_: ObjectId) -> Any:
+    async def get_item_by_id(
+        self, id_: ObjectId, *, session: AsyncIOMotorClientSession | None = None
+    ) -> Any:
         """Retrieves an item from the repository by its unique identifier.
 
         Args:
             id_ (ObjectId): The unique identifier of the item.
+            session (AsyncIOMotorClientSession | None): Defines a client session
+            if operation is transactional. Defaults to None.
 
         Returns:
             Any: The retrieved item.
@@ -40,14 +47,18 @@ class BaseRepository(abc.ABC):
         raise NotImplementedError
 
     @abc.abstractmethod
-    async def create_items(self, items: List[Dict[str, Any]]) -> List[ObjectId]:
-        """Creates bulk items in the repository.
+    async def create_item(
+        self, item: Any, *, session: AsyncIOMotorClientSession | None = None
+    ) -> Any:
+        """Create a new item in repository.
 
         Args:
-            items (List[Dict[str, Any]]): Items to be created.
+            item (Any): The data for the new item.
+            session (AsyncIOMotorClientSession | None): Defines a client session
+            if operation is transactional. Defaults to None.
 
         Returns:
-            List[ObjectId]: The created items.
+            Any: The ID of created item.
 
         Raises:
             NotImplementedError: This method must be implemented by subclasses.
@@ -56,8 +67,37 @@ class BaseRepository(abc.ABC):
         raise NotImplementedError
 
     @abc.abstractmethod
-    async def delete_all_items(self) -> None:
+    async def create_items(
+        self,
+        items: List[Dict[str, Any]],
+        *,
+        session: AsyncIOMotorClientSession | None = None,
+    ) -> List[Any]:
+        """Creates bulk items in the repository.
+
+        Args:
+            items (List[Dict[str, Any]]): Items to be created.
+            session (AsyncIOMotorClientSession | None): Defines a client session
+            if operation is transactional. Defaults to None.
+
+        Returns:
+            List[Any]: The IDs of created items.
+
+        Raises:
+            NotImplementedError: This method must be implemented by subclasses.
+
+        """
+        raise NotImplementedError
+
+    @abc.abstractmethod
+    async def delete_all_items(
+        self, *, session: AsyncIOMotorClientSession | None = None
+    ) -> None:
         """Deletes all items from the repository.
+
+        Args:
+            session (AsyncIOMotorClientSession | None): Defines a client session
+            if operation is transactional. Defaults to None.
 
         Raises:
             NotImplementedError: This method must be implemented by subclasses.
