c77ac54da67991281c6119cfa14a0ea616f717b0
Valentyn Malenchak <valentyn17@gmail.com>
2024-02-12 18:10:11 +0200

    [SHIBU-0041]: Implement product model

---
+++ app/api/v1/models/product.py
@@ -0,0 +1,23 @@
+"""Module that contains product domain models."""
+
+from datetime import datetime
+from typing import Annotated, Any, Dict
+
+from bson import ObjectId
+
+from app.api.v1.models import ObjectIdAnnotation, ObjectIdModel
+
+
+class Product(ObjectIdModel):
+    """Product model."""
+
+    name: str
+    synopsis: str
+    description: str
+    quantity: int
+    category_id: Annotated[ObjectId, ObjectIdAnnotation]
+    available: bool  # defines if product should be shown for customers
+    html_body: str | None
+    parameters: Dict[str, Any]
+    created_at: datetime
+    updated_at: datetime | None
