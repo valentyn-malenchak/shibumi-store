e6f3d27d19ab60c0966d5225335e3b7f9af160c3
Valentyn Malenchak <valentyn17@gmail.com>
2024-02-15 21:00:35 +0200

    [SHIBU-0043]: Refactor repositories to avoid code duplication

--- app/api/v1/repositories/product.py
+++ app/api/v1/repositories/product.py
@@ -1,9 +1,6 @@
 """Module that contains product repository class."""
 
-from typing import Any, Dict, List, Mapping
-
-from bson import ObjectId
-from motor.motor_asyncio import AsyncIOMotorClientSession
+from typing import Any, Mapping
 
 from app.api.v1.repositories import BaseRepository
 from app.services.mongo.constants import MongoCollectionsEnum
@@ -14,150 +11,16 @@ class ProductRepository(BaseRepository):
 
     _collection_name: str = MongoCollectionsEnum.PRODUCTS.value
 
-    async def get(
-        self,
-        *_: Any,
-        session: AsyncIOMotorClientSession | None = None,
-    ) -> List[Any]:
-        """Retrieves a list of products based on parameters.
-
-        Args:
-            _ (Any): Parameters for list filtering, searching, sorting and pagination.
-            session (AsyncIOMotorClientSession | None): Defines a client session
-            if operation is transactional. Defaults to None.
-
-        Returns:
-            List[Any]: The retrieved list of products.
-
-        Raises:
-            NotImplementedError: This method is not implemented.
-
-        """
-        raise NotImplementedError
-
     @staticmethod
-    async def _get_list_query_filter(*_: Any) -> Mapping[str, Any]:
+    async def _get_list_query_filter(*_: Any, **__: Any) -> Mapping[str, Any]:
         """Returns a query filter for list.
 
         Args:
-            _ (Any): Parameters for list filtering and searching.
+            _ (Any): Parameters for list searching.
+            __ (Any): Parameters for list filtering.
 
         Returns:
             (Mapping[str, Any]): List query filter.
-
-        Raises:
-            NotImplementedError: This method is not implemented.
-
-        """
-        raise NotImplementedError
-
-    async def count(
-        self, *_: Any, session: AsyncIOMotorClientSession | None = None
-    ) -> int:
-        """Counts documents based on parameters.
-
-        Args:
-            _ (Any): Parameters for list filtering and searching.
-            session (AsyncIOMotorClientSession | None): Defines a client session
-            if operation is transactional. Defaults to None.
-
-        Returns:
-            int: Count of documents.
-
-        Raises:
-            NotImplementedError: This method is not implemented.
-
-        """
-        raise NotImplementedError
-
-    async def get_by_id(
-        self, id_: ObjectId, *, session: AsyncIOMotorClientSession | None = None
-    ) -> Mapping[str, Any] | None:
-        """Retrieves a product from the repository by its unique identifier.
-
-        Args:
-            id_ (ObjectId): The unique identifier of the product.
-            session (AsyncIOMotorClientSession | None): Defines a client session
-            if operation is transactional. Defaults to None.
-
-        Returns:
-            Mapping[str, Any] | None: The retrieved product.
-
-        """
-
-        return await self._mongo_service.find_one(
-            collection=self._collection_name, filter_={"_id": id_}, session=session
-        )
-
-    async def create(
-        self, item: Dict[str, Any], *, session: AsyncIOMotorClientSession | None = None
-    ) -> Any:
-        """Creates a new product in repository.
-
-        Args:
-            item (Dict[str, Any]): The data for the new product.
-            session (AsyncIOMotorClientSession | None): Defines a client session
-            if operation is transactional. Defaults to None.
-
-        Returns:
-            Any: The ID of created product.
-
-        """
-        return await self._mongo_service.insert_one(
-            collection=self._collection_name, document=item, session=session
-        )
-
-    async def create_many(
-        self,
-        items: List[Dict[str, Any]],
-        *,
-        session: AsyncIOMotorClientSession | None = None,
-    ) -> List[Any]:
-        """Creates bulk products in the repository.
-
-        Args:
-            items (List[Dict[str, Any]]): Products to be created.
-            session (AsyncIOMotorClientSession | None): Defines a client session
-            if operation is transactional. Defaults to None.
-
-        Returns:
-            List[Any]: The IDs of created products.
-
-        Raises:
-            NotImplementedError: This method is not implemented.
-
-        """
-        raise NotImplementedError
-
-    async def delete_all(
-        self, *, session: AsyncIOMotorClientSession | None = None
-    ) -> None:
-        """Deletes all products from the repository.
-
-        Args:
-            session (AsyncIOMotorClientSession | None): Defines a client session
-            if operation is transactional. Defaults to None.
-
-        Raises:
-            NotImplementedError: This method is not implemented.
-
-        """
-        raise NotImplementedError
-
-    async def update_by_id(
-        self,
-        id_: ObjectId,
-        item: Dict[str, Any],
-        *,
-        session: AsyncIOMotorClientSession | None = None,
-    ) -> None:
-        """Updates a product in repository.
-
-        Args:
-            id_ (ObjectId): The unique identifier of the product.
-            item (Dict[str, Any]): Data to update product.
-            session (AsyncIOMotorClientSession | None): Defines a client session
-            if operation is transactional. Defaults to None.
 
         Raises:
             NotImplementedError: This method is not implemented.
--- app/api/v1/repositories/role_scopes.py
+++ app/api/v1/repositories/role_scopes.py
@@ -1,8 +1,7 @@
 """Module that contains role scopes repository class."""
 
-from typing import Any, Dict, List, Mapping
+from typing import Any, List, Mapping
 
-from bson import ObjectId
 from motor.motor_asyncio import AsyncIOMotorClientSession
 
 from app.api.v1.repositories import BaseRepository
@@ -14,72 +13,16 @@ class RoleScopesRepository(BaseRepository):
 
     _collection_name: str = MongoCollectionsEnum.ROLES_SCOPES.value
 
-    async def get(
-        self, *_: Any, session: AsyncIOMotorClientSession | None = None
-    ) -> List[Any]:
-        """Retrieves a list of roles-scopes based on parameters.
-
-        Args:
-            _ (Any): Parameters for list filtering, searching, sorting and pagination.
-            session (AsyncIOMotorClientSession | None): Defines a client session
-            if operation is transactional. Defaults to None.
-
-        Returns:
-            List[Any]: The retrieved list of roles-scopes.
-
-        Raises:
-            NotImplementedError: This method is not implemented.
-
-        """
-        raise NotImplementedError
-
     @staticmethod
-    async def _get_list_query_filter(*_: Any) -> Mapping[str, Any]:
+    async def _get_list_query_filter(*_: Any, **__: Any) -> Mapping[str, Any]:
         """Returns a query filter for list.
 
         Args:
-            _ (Any): Parameters for list filtering and searching.
+            _ (Any): Parameters for list searching.
+            __ (Any): Parameters for list filtering.
 
         Returns:
             (Mapping[str, Any]): List query filter.
-
-        Raises:
-            NotImplementedError: This method is not implemented.
-
-        """
-        raise NotImplementedError
-
-    async def count(
-        self, *_: Any, session: AsyncIOMotorClientSession | None = None
-    ) -> int:
-        """Counts documents based on parameters.
-
-        Args:
-            _ (Any): Parameters for list filtering and searching.
-            session (AsyncIOMotorClientSession | None): Defines a client session
-            if operation is transactional. Defaults to None.
-
-        Returns:
-            int: Count of documents.
-
-        Raises:
-            NotImplementedError: This method is not implemented.
-
-        """
-        raise NotImplementedError
-
-    async def get_by_id(
-        self, id_: ObjectId, *, session: AsyncIOMotorClientSession | None = None
-    ) -> Any:
-        """Retrieves a role-scopes from the repository by its unique identifier.
-
-        Args:
-            id_ (ObjectId): The unique identifier of the role-scopes.
-            session (AsyncIOMotorClientSession | None): Defines a client session
-            if operation is transactional. Defaults to None.
-
-        Returns:
-            Any: The retrieved role-scopes.
 
         Raises:
             NotImplementedError: This method is not implemented.
@@ -113,80 +56,3 @@ class RoleScopesRepository(BaseRepository):
         )
 
         return [scope["_id"] for scope in scopes]
-
-    async def create(
-        self, item: Any, *, session: AsyncIOMotorClientSession | None = None
-    ) -> Any:
-        """Create a new role-scopes in repository.
-
-        Args:
-            item (Any): The data for the new role-scopes.
-            session (AsyncIOMotorClientSession | None): Defines a client session
-            if operation is transactional. Defaults to None.
-
-        Returns:
-            Any: The ID of created role-scopes.
-
-        Raises:
-            NotImplementedError: This method is not implemented.
-
-        """
-        raise NotImplementedError
-
-    async def create_many(
-        self,
-        items: List[Dict[str, Any]],
-        *,
-        session: AsyncIOMotorClientSession | None = None,
-    ) -> List[Any]:
-        """Creates bulk roles-scopes in the repository.
-
-        Args:
-            items (List[Dict[str, Any]]): Roles-scopes to be created.
-            session (AsyncIOMotorClientSession | None): Defines a client session
-            if operation is transactional. Defaults to None.
-
-        Returns:
-            List[Any]: The IDs of created roles-scopes.
-
-        Raises:
-            NotImplementedError: This method is not implemented.
-
-        """
-        raise NotImplementedError
-
-    async def delete_all(
-        self, *, session: AsyncIOMotorClientSession | None = None
-    ) -> None:
-        """Deletes all roles-scopes from the repository.
-
-        Args:
-            session (AsyncIOMotorClientSession | None): Defines a client session
-            if operation is transactional. Defaults to None.
-
-        Raises:
-            NotImplementedError: This method is not implemented.
-
-        """
-        raise NotImplementedError
-
-    async def update_by_id(
-        self,
-        id_: ObjectId,
-        item: Dict[str, Any],
-        *,
-        session: AsyncIOMotorClientSession | None = None,
-    ) -> None:
-        """Updates a role-scopes in repository.
-
-        Args:
-            id_ (ObjectId): The unique identifier of the role-scopes.
-            item (Dict[str, Any]): Data to update role-scopes.
-            session (AsyncIOMotorClientSession | None): Defines a client session
-            if operation is transactional. Defaults to None.
-
-        Raises:
-            NotImplementedError: This method is not implemented.
-
-        """
-        raise NotImplementedError
